5859722217837dd579104f6193eb6c1e3dad317a
ilatrofimenkov2-lab <ilatrofimenkov2@gmail.com>
2026-05-26 23:38:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,52 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+)
 
 TOKEN = "8956006401:AAEUArB7IsF-ceqJoaTyOajUC_XMdLJ3s2M"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="💎 Купить")],
+        [KeyboardButton(text="📦 Тарифы")],
+        [KeyboardButton(text="📞 Поддержка")],
+    ],
+    resize_keyboard=True
+)
+
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Бот работает ✅")
+    await message.answer(
+        "🔥 KRaks Store\n\n"
+        "💎 MAGIC для OXIDE",
+        reply_markup=keyboard
+    )
+
+@dp.message()
+async def buttons(message: Message):
+
+    if message.text == "📦 Тарифы":
+        await message.answer(
+            "📦 Тарифы:\n\n"
+            "💎 1 День — 280₽"
+        )
+
+    elif message.text == "💎 Купить":
+        await message.answer(
+            "📞 Для покупки: @No_Fake_Kraks"
+        )
+
+    elif message.text == "📞 Поддержка":
+        await message.answer(
+            "📩 Поддержка: @No_Fake_Kraks"
+        )
 
 async def main():
     await dp.start_polling(bot)
