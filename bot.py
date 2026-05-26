63b7e1b2166b6dcf42cc6cd7ee3a6ecb959d4ea0
ilatrofimenkov2-lab <ilatrofimenkov2@gmail.com>
2026-05-26 21:56:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,46 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 
 TOKEN = "8956006401:AAEUArB7IsF-ceqJoaTyOajUC_XMdLJ3s2M"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher() 
+main_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="💎 Купить MAGIC",
+                url="https://t.me/No_Fake_Kraks"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="📦 Тарифы",
+                callback_data="prices"
+            ),
+            InlineKeyboardButton(
+                text="⭐ Отзывы",
+                callback_data="reviews"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="📖 Инструкция",
+                callback_data="guide"
+            ),
+            InlineKeyboardButton(
+                text="📞 Поддержка",
+                url="https://t.me/No_Fake_Kraks"
+            ),
+        ],
+    ]
+)
 
 
 @dp.message(CommandStart())
@@ -16,7 +50,8 @@ async def start(message: Message):
         "🔥 Добро пожаловать в Kraks Store\n\n"
         "💎 MAGIC для OXIDE\n"
         "💰 Цена: 280₽ / 1 день\n\n"
-        "📞 Покупка: @No_Fake_Kraks"
+        "📞 покупка: @No_Fake_Kraks"
+    reply_markup=main_keyboard
     )
 
 
